f226f8a5664d84e3465df2559dac13416c0a102d
VictoriaLatham <victorialatham@redcross.org.uk>
2023-06-14 12:12:20 +0100

    added R scripts and produced graphs

---
+++ r/mapping_heat_alerts.R
@@ -0,0 +1,82 @@
+# Load libraries
+library(readxl)
+library(tidyverse)
+library(geographr)
+library(demographr)
+library(sf)
+library(ggplot2)
+source("r/themes.R")
+
+# Load data
+warnings <- readxl::read_xlsx("data/heat_health_alerts.xlsx")
+
+# Clean data
+warnings_clean <- warnings |> 
+  pivot_longer(cols = 6:14,
+               names_to = "area",
+               values_to = "alert") |> 
+  drop_na(alert) |> 
+  mutate(alert_level = str_to_title(alert_level)) |> 
+  left_join(geographr::boundaries_region21, by=c("area" = "region21_name")) |>  # using old 2021 region file as map is going to be small so do not need updated 2022 shapes
+  st_as_sf()
+  
+# Create map
+# Alert 1
+warnings_clean |>
+  filter(alert_number == "1") |> 
+  ggplot() +
+  geom_sf(aes(fill = alert_level))  +
+  labs(
+    title = "Heat-Health Alert \n 9th June 23 - 12th June 23",
+    caption = "Source: UKHSA / Met Office"
+  ) +
+  scale_fill_manual(values = c("Red" = "#CC0033", "Amber" = "#FF9900", "Yellow" = "#FFE923", "Green" = "white")) +
+  theme_map() 
+
+# Save output
+ggsave("outputs/heat_alert_1.png", width = 250, height = 200, units = "mm")
+
+# Alert 2
+warnings_clean |>
+  filter(alert_number == "2") |> 
+  ggplot() +
+  geom_sf(aes(fill = alert_level))  +
+  labs(
+    title = "Heat-Health Alert \n 9th June 23 - 12th June 23",
+    caption = "Source: UKHSA / Met Office"
+  ) +
+  scale_fill_manual(values = c("Red" = "#CC0033", "Amber" = "#FF9900", "Yellow" = "#FFE923", "Green" = "white")) +
+  theme_map() 
+
+# Save output
+ggsave("outputs/heat_alert_2.png", width = 250, height = 200, units = "mm")
+
+# Alert 3
+warnings_clean |>
+  filter(alert_number == "3") |> 
+  ggplot() +
+  geom_sf(aes(fill = alert_level))  +
+  labs(
+    title = "Heat-Health Alert \n 9th June 23 - 13th June 23",
+    caption = "Source: UKHSA / Met Office"
+  ) +
+  scale_fill_manual(values = c("Red" = "#CC0033", "Amber" = "#FF9900", "Yellow" = "#FFE923", "Green" = "white")) +
+  theme_map() 
+
+# Save output
+ggsave("outputs/heat_alert_3.png", width = 250, height = 200, units = "mm")
+
+# Alert 1
+warnings_clean |>
+  filter(alert_number == "4") |> 
+  ggplot() +
+  geom_sf(aes(fill = alert_level))  +
+  labs(
+    title = "Heat-Health Alert \n 13th June 23 - 19th June 23",
+    caption = "Source: UKHSA / Met Office"
+  ) +
+  scale_fill_manual(values = c("Red" = "#CC0033", "Amber" = "#FF9900", "Yellow" = "#FFE923", "Green" = "white")) +
+  theme_map() 
+
+# Save output
+ggsave("outputs/heat_alert_4.png", width = 250, height = 200, units = "mm")
---
+++ r/themes.R
@@ -0,0 +1,91 @@
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = "Arial", color = "#22211d"),
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#ffffff", color = NA),
+      panel.background = element_rect(fill = "#ffffff", color = NA),
+      legend.background = element_rect(fill = "#ffffff", color = NA),
+      panel.border = element_blank(),
+      # Add labs elements
+      legend.position = "none",
+      legend.title = element_text(size = 12),
+      legend.text = element_text(size = 10, hjust = 0),
+      plot.title = element_text(size = 18, hjust = 0.5),
+      plot.subtitle = element_text(
+        size = 10, hjust = 0.5,
+        margin = margin(
+          b = 0.2,
+          t = 0.2,
+          l = 2,
+          unit = "cm"
+        ),
+        debug = F
+      ),
+      # captions
+      plot.caption = element_text(
+        size = 12,
+        hjust = 0.5,
+        margin = margin(
+          t = 0.2,
+          b = 0,
+          unit = "cm"
+        ),
+        color = "#939184"
+      ),
+      ...
+    )
+}
+
+theme_chart <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = "Arial", color = "#22211d"),
+      axis.line = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#ffffff", color = NA),
+      panel.background = element_rect(fill = "#ffffff", color = NA),
+      legend.background = element_rect(fill = "#ffffff", color = NA),
+      panel.border = element_blank(),
+      # Add labs elements
+      plot.title = element_text(size = 20, hjust = 0.5),
+      legend.text=element_text(size = 14),
+      legend.title=element_text(size = 16, hjust = 0),
+      axis.text = element_text(size = 14),
+      plot.subtitle = element_text(
+        size = 10, hjust = 0.5,
+        margin = margin(
+          b = 0.2,
+          t = 0.2,
+          l = 2,
+          unit = "cm"
+        ),
+        debug = F
+      ),
+      # captions
+      plot.caption = element_text(
+        size = 14,
+        hjust = 0,
+        margin = margin(
+          t = 0.2,
+          b = 0,
+          unit = "cm"
+        ),
+        color = "#939184"
+      ),
+      ...
+    )
+}
